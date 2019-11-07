1c3cbf624f5f4c253879d0748015193441656c3c
Iago Pampillón Vieitez <ipampillonvieitez@nautilus27.ad.danielcastelao.org>
2019-11-07 13:37:13 +0100

    ejemplo bucles

---
+++ src/exemplobucles/ExemploBucles.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplobucles;
+
+/**
+ *
+ * @author ipampillonvieitez
+ */
+public class ExemploBucles {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Metodos obx= new Metodos();
+        obx.verDatos();
+    }
+    
+}
